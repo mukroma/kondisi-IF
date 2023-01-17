b27702f02113a79662005600eaaa661bcd9d79fd
mukroma <122344043+mukroma@users.noreply.github.com>
2023-01-17 12:46:31 +0700

    Add files via upload

---
+++ if.py
@@ -0,0 +1,10 @@
+print ("nilai tugas siswa")
+nilai = int(input("masukan nilai tugas:"))
+if nilai >= 80:
+   print("A")
+elif nilai >= 70:
+   print("B")
+elif nilai >= 60:
+   print("C")
+else:
+   print("D")
